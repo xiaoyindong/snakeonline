e607cb3cd5b869a9f1f72fe575eee0a58cfd2973
yindong <812748570@qq.com>
2020-05-31 14:33:55 +0800

    feat: 贪食蛇

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,9 +1,7 @@
 /**
  * 图文咨询主页
  */
-import ReactDOM from 'react-dom';
-import Home from './Home.jsx';
 import '@global';
 import 'lib-flexible';
 
-ReactDOM.render(<Home />, document.getElementById('App'));
+alert(123)
--- src/pages/order_make/Main.jsx
+++
@@ -1,6 +0,0 @@
-
-import './style.less';
-
-export default () => {
-    return <div id="order_make">123</div>
-}
