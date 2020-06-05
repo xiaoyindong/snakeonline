ea07d6ec10b0719164fe937b176fba6996223c67
yindong <812748570@qq.com>
2020-06-05 22:18:54 +0800

    feat: 213

--- src/pages/home/Play.js
+++ src/pages/home/Play.js
@@ -1,16 +1,35 @@
 class Play {
-    constructor(name) {
+    constructor(name, x, y) {
         this.name = name;
+        this.x = x || 60;
+        this.y = y || 50;
         this.render();
+        this.drawing();
+
     }
 
     render() {
-        const ele = document.createElement('div');
-        ele.id = 'play';
-        const html = `<div>123</div>`;
-        ele.innerHTML = html;
-        document.body.appendChild(ele);
-
+        const canvas = document.createElement('canvas');
+        canvas.id = 'play_style';
+        canvas.width = window.innerWidth;
+        canvas.height = window.innerHeight;
+        this.canvas = canvas;
+        this.ctx = canvas.getContext('2d');
+        document.body.appendChild(canvas);
+    }
+    drawing() {
+        this.ctx.clearRect(0, 0, window.innerWidth, window.innerHeight);
+        this.ctx.beginPath();
+        this.ctx.arc(this.x, this.y, 10, 0, Math.PI * 2, false);
+        this.ctx.fillStyle = "rgba(192,80,77,0.7)";//半透明的红色
+        this.ctx.fill();
+        this.ctx.strokeStyle = "rgba(192,80,77,1)";//红色
+        this.ctx.stroke();
+    }
+    moveTo(diffX = 0, diffY = 0) {
+        this.x = this.x + diffX;
+        this.y = this.y + diffY;
+        this.drawing();
     }
 }
 
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -9,16 +9,20 @@ import Background from './Background';
 import Play from './Play';
 
 const back = new Background();
-const canvas = back.getTarget();
 
-const ctx = canvas.getContext('2d');
-ctx.fillStyle="#FF0000";
-ctx.fillRect(0,0,150,75);
-// anmition.add('1', () => {
-//     console.log(123);
-// })
+// const canvas = back.getTarget();
 
-new Play();
+// const ctx = canvas.getContext('2d');
+// ctx.fillStyle="#FF0000";
+// ctx.fillRect(0,0,150,75);
+anmition.add('1', () => {
+    // play1.moveTo(Math.round(Math.random() * 2), Math.round(Math.random() * 2));
+})
+
+const play1 = new Play();
+
+
+
 
 // const anmition = new Timer();
 
